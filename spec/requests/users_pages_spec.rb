28b4ba72cd8e51f76544469634b9e1b8a47540c2
Goh Matsumoto <urimaro@gmail.com>
2014-05-21 19:05:51 +0900

    add tests for the content and title of Signup page

--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "UsersPages" do
-  describe "GET /users_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_pages_index_path
-      response.status.should be(200)
-    end
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 end
